25cdafb8f0565e86936bb56f5e0cbddb0edd17b0
Dan Zaratsian <dtz001@gmail.com>
2022-11-30 16:49:01 +0000

    Added deployments

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
-docker build -t fuse .
+export GCP_PROJECT_ID="stealth-air-23412"
+export ARTIFACT_REPO_NAME="sftp-for-gcs"
+export ARTIFACT_REPO_LOCATION="us-central1"
+export APP_IMAGE="sftp-for-gcs"
+
+# Create Artifact Repo
+gcloud artifacts repositories create $ARTIFACT_REPO_NAME --repository-format=docker \
+--location=$ARTIFACT_REPO_LOCATION --description="SFTP for GCS Repository"
+
+# Cloud Build
+gcloud builds submit --config=cloudbuild.yaml \
+  --substitutions=_LOCATION=$ARTIFACT_REPO_LOCATION,_REPOSITORY=$ARTIFACT_REPO_NAME,_IMAGE=$APP_IMAGE .
+
